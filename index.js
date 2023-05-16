9e3b88b46b4f5da40354935b4d8a0ac892f2c086
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-16 23:38:15 +0600

    dynamic data find added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,17 @@ async function run() {
         const cursor = servicesCollection.find()
         const result =await cursor.toArray()
         res.send(result)
+    })
+    app.get('/services/:id',async(req,res)=>{
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id)};
+         const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: {  title: 1, price: 1 },
+          };
+         const result = await servicesCollection.findOne(query,options)
+         res.send(result)
+
     })
 
     const carDoctorDB =
